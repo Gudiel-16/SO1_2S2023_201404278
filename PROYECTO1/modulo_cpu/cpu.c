36dd379cf89c6b4de372d9853251f7c98f5b0f42
Gudiel-16 <2834237140101@ingenieria.usac.edu.gt>
2023-09-06 02:41:38 -0600

    [UPDATE] Modulo CPU, para obtener datos desde golang

--- PROYECTO1/modulo_cpu/cpu.c
+++ PROYECTO1/modulo_cpu/cpu.c
@@ -3,6 +3,7 @@
 // Info del kernel en tiempo real
 #include <linux/kernel.h>
 #include <linux/sched.h>
+#include <linux/sched/signal.h>
 
 // Headers para modulos
 #include <linux/init.h>
@@ -14,8 +15,10 @@
 #include <linux/seq_file.h>
 // Para get_mm_rss
 #include <linux/mm.h>
+#include <linux/jiffies.h>
 
 struct task_struct *cpu; // Estructura que almacena info del cpu
+struct task_struct *current_cpu;
 
 // Almacena los procesos
 struct list_head *lstProcess;
@@ -25,11 +28,22 @@ unsigned long rss;
 
 // const long megabyte = 1024 * 1024;
 
+// unsigned long long total_time = 0;
+// unsigned long long total_usage = 0;
+// unsigned long long porc_usage = 0;
+// unsigned long long aux = 0;
+// unsigned long long aux2 = 0;
+// unsigned long long prev_total_time = 0;
+
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Modulo de CPU para proyecto1");
 MODULE_AUTHOR("Gudiel 16");
 
 static int escribir_archivo(struct seq_file *archivo, void *v) {
+
+    // total_time = 0;
+    // total_usage = 0;
+
     for_each_process(cpu) {
         seq_printf(archivo, "PID%d", cpu->pid); // Identificador del proceso: PID1
         seq_printf(archivo, ",");
@@ -54,7 +68,7 @@ static int escribir_archivo(struct seq_file *archivo, void *v) {
 
         list_for_each(lstProcess, &(cpu->children)) {
             child = list_entry(lstProcess, struct task_struct, sibling);
-            seq_printf(archivo, "CHILD:%d", child->pid); // Identificador del proceso hijo: CHILD1
+            seq_printf(archivo, "CHILD%d", child->pid); // Identificador del proceso hijo: CHILD1
             seq_printf(archivo, ".");
             seq_printf(archivo, "%d", child->pid); // Identificador del proceso hijo (solo numero): 1
             seq_printf(archivo, ".");
@@ -72,8 +86,34 @@ static int escribir_archivo(struct seq_file *archivo, void *v) {
             seq_printf(archivo, ".");
 
             seq_printf(archivo, "%d", child->cred->user->uid);
+            seq_printf(archivo, ".");
         }
+
+        seq_printf(archivo, ";"); // Fin de cada proceso
+
+        // total_time += cpu->utime + cpu->stime;
+
     }
+
+    // seq_printf(archivo, ";;siuuu;;");
+    // if(prev_total_time != 0){
+    //     total_usage = total_time - prev_total_time;
+    //     aux = total_usage * 100;
+    //     aux2 = jiffies_to_msecs(1) * HZ;
+    //     total_usage = aux / aux2;
+
+    //     // porc_usage = total_usage * 100;
+    //     // porc_usage = porc_usage / HZ;
+    //     seq_printf(archivo, "%llu", total_time);
+    //     seq_printf(archivo, ",");
+    //     seq_printf(archivo, "%llu", prev_total_time);
+    //     seq_printf(archivo, ",");
+    //     seq_printf(archivo, "%llu", total_usage);
+    // }
+
+    // seq_printf(archivo, ";;siuuu;;");
+
+    // prev_total_time = total_time;
 
     return 0;
 }
