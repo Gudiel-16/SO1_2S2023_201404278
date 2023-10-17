83e4e6d2303b66005e6243487acf30ecde2230c6
Gudiel-16 <2834237140101@ingenieria.usac.edu.gt>
2023-10-17 01:42:13 -0600

    [UPDATE] Frontend, integracion de socket.io

--- PROYECTO2/frontend/src/pages/DinamicPage.jsx
+++ PROYECTO2/frontend/src/pages/DinamicPage.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import socket from '../socket/Socket';
 
 const DinamicPage = () => {
+
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    // no-op if the socket is already connected
+    socket.connect();
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+
+    function onFooEvent(value) {
+      console.log(value);
+      setFooEvents(value);
+    }
+
+    socket.on("datadinamic", onFooEvent);
+
+    return () => {
+      socket.off('datadinamic', onFooEvent);
+    }
+  }, [fooEvents]);
+
   return (
     <div>DinamicPage</div>
   )
