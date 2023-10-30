7c08d02a7b6f601e91524410fa01ccd831233d56
Gudiel-16 <2834237140101@ingenieria.usac.edu.gt>
2023-10-30 02:20:15 -0600

    [FIX] Bug de socket.io, agregando que conecte al protocolo http

--- PROYECTO2/frontend/src/socket/Socket.jsx
+++ PROYECTO2/frontend/src/socket/Socket.jsx
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const URL = import.meta.env.VITE_IP_NODE + ":5003";
+const URL = "http://" + import.meta.env.VITE_IP_NODE + ":5003";
 
 const socket = io(URL, {
     autoConnect: false // no connect automatically
